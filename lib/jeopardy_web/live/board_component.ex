b5d8c8eaa2d24b750ba4ff5050374d25e158a37a
Ryan Young <ryany@spotify.com>
2020-05-10 17:43:10 -0400

    board component cleanup

--- lib/jeopardy_web/live/board_component.ex
+++ lib/jeopardy_web/live/board_component.ex
@@ -11,23 +11,13 @@ defmodule JeopardyWeb.BoardComponent do
                 <%= category %>
             </div>
             <%= for clue <- clues do %>
-                <%= if @view == "trebek" do %>
-                  <%= if should_display_clue(clue) do %>
-                    <div class="clue" style="background:blue; color:gold; height:100px; display:grid; place-items:center;"
-                    phx-click="click_clue" phx-value-clue_id=<%= clue.id %>>
-                      $<%= clue.value %>
-                    </div>
-                  <% else %>
-                    <div class="clue" style="background:blue; color:gold; height:100px; display:grid; place-items:center;">
-                    </div>
-                  <% end %>
-                <% else %>
-                  <div class="clue">
-                    <%= if should_display_clue(clue) do %>
-                        $<%= clue.value %>
-                    <% end %>
+                <%= if should_display_clue(clue) do %>
+                  <div class="clue" <%= if @view == "trebek" do %>phx-click="click_clue" phx-value-clue_id=<%= clue.id %><% end %> >
+                    $<%= clue.value %>
                   </div>
-              <% end %>
+                <% else %>
+                  <div class="clue"></div>
+                <% end %>
             <% end %>
         </div>
     <% end %>
