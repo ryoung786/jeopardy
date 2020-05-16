89b96dd930942ebe249b7edb195b7cefbc3a5fed
Ryan Young <ryany@spotify.com>
2020-05-15 22:23:40 -0400

    adding jarchive json files

--- lib/jeopardy/j_archive.ex
+++ lib/jeopardy/j_archive.ex
@@ -5,43 +5,18 @@ defmodule Jeopardy.JArchive do
 
   import Ecto.Query, warn: false
   alias Jeopardy.Repo
-  alias Jeopardy.JArchive.Game, as: JGame
-  alias Jeopardy.JArchive.Clue, as: JClue
   alias Jeopardy.Games.{Game, Clue}
 
+  @archive_dir Path.join(:code.priv_dir(:jeopardy), "jarchive")
+
   def random_game() do
-    # we use the count of the number of valid games to create a random offset
-    num_games = (from g in JGame, select: count(g.id), where: not is_nil(g.final_jeopardy_category)) |> Repo.one()
-    offset = :rand.uniform(num_games)-1
-
-    JGame
-    |> where([g], not is_nil(g.final_jeopardy_category))
-    |> offset(^offset)
-    |> limit(1)
-    |> preload([c], [:clues])
-    |> Repo.one
+    Path.wildcard(Path.join(@archive_dir, "*.json"))
+    |> Enum.random
+    |> File.read!
+    |> Jason.decode!
   end
 
-  def specific_game(id), do: Repo.get!(JGame, id)
-
-  def clues_by_category(%JGame{} = game, round) when round in [:jeopardy, :double_jeopardy] do
-    clues = from(c in JClue,
-      where: c.game_id == ^game.id,
-      where: c.round == ^Atom.to_string(round),
-      order_by: [asc: c.value])
-      |> Repo.all
-    Enum.map(game.jeopardy_round_categories, fn category ->
-      [category: category,
-       clues: clues |> Enum.filter(fn clue -> clue.category == category end)]
-    end)
-  end
-
-  def final_jeopardy_clue(%JGame{} = game) do
-    from(c in JClue,
-      where: c.game_id == ^game.id,
-      where: c.round == "final_jeopardy")
-    |> Repo.one
-  end
+  def specific_game(id), do: Jason.decode!(File.read!(Path.join(@archive_dir, "#{id}.json")))
 
   def load_into_game(%Game{} = game) do
     jgame = random_game()
@@ -55,15 +30,15 @@ defmodule Jeopardy.JArchive do
 
   defp load_into_game_changeset(game, jgame) do
     Game.changeset(
-      game,
-      Map.from_struct(jgame) |> Map.put(:jarchive_game_id, jgame.id)
+      game, jgame |> Map.put("jarchive_game_id", jgame["id"])
     )
   end
 
-  defp load_clues(%Game{} = game, %JGame{} = jgame) do
-    clues = Enum.map(jgame.clues, fn jclue ->
+  defp load_clues(%Game{} = game, jgame) do
+    clues = jgame["jeopardy"] ++ jgame["double_jeopardy"] ++ [jgame["final_jeopardy"]]
+    clues = Enum.map(clues, fn jclue ->
       clue =
-        Clue.changeset(%Clue{}, Map.from_struct(jclue))
+        Clue.changeset(%Clue{}, jclue)
         |> Ecto.Changeset.apply_changes()
 
       Ecto.build_assoc(game, :clues, clue)
