f033790630217addb4319e0b6166e9332b399237
Ryan Young <ryany@spotify.com>
2020-05-15 22:38:40 -0400

    distillery init

--- mix.exs
+++ mix.exs
@@ -47,7 +47,8 @@ defmodule Jeopardy.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:calendar, "~> 1.0.0"}
+      {:calendar, "~> 1.0.0"},
+      {:distillery, "~> 2.1"}
     ]
   end
 
