95cec29678229b3752e8f90762a4659872b0d7eb
Ryan Young <ryany@spotify.com>
2020-05-20 13:26:02 -0400

    fixed bug, can now move from j round to dj round

--- lib/jeopardy/fsm/jeopardy/recapping_scores.ex
+++ lib/jeopardy/fsm/jeopardy/recapping_scores.ex
@@ -3,7 +3,7 @@ defmodule Jeopardy.FSM.Jeopardy.RecappingScores do
   alias Jeopardy.GameState
 
   def handle(_, _, %Game{} = g) do
-    case g.round do
+    case g.status do
       "jeopardy" -> GameState.update_game_status(g.code, "jeopardy", "double_jeopardy", "revealing_board")
       "double_jeopardy" -> GameState.update_game_status(g.code, "double_jeopardy", "final_jeopardy", "revealing_category")
     end
