2cbc115e59793f60260b4b27c9f942b3982e3cef
Ryan Young <ryany@spotify.com>
2020-05-21 22:37:43 -0400

    little nicer styling

--- lib/jeopardy_web/components/trebek/jeopardy/revealing_board.ex
+++ lib/jeopardy_web/components/trebek/jeopardy/revealing_board.ex
@@ -3,9 +3,10 @@ defmodule JeopardyWeb.Components.Trebek.Jeopardy.RevealingBoard do
   require Logger
 
   @impl true
-  def mount(socket) do
-    {:ok, assign(socket, active_category_num: 0)}
-  end
+  def mount(socket), do: {:ok, assign(socket, active_category_num: 0)}
+
+  @impl true
+  def render(assigns), do: JeopardyWeb.TrebekView.render(tpl_path(assigns), assigns)
 
   @impl true
   def update(assigns, socket) do
@@ -18,13 +19,9 @@ defmodule JeopardyWeb.Components.Trebek.Jeopardy.RevealingBoard do
   end
 
   @impl true
-  def render(assigns) do
-    JeopardyWeb.TrebekView.render(tpl_path(assigns), assigns)
-  end
-
-  @impl true
   def handle_event("next_category", _params, socket) do
     active_category_num = socket.assigns.active_category_num + 1
+    broadcast(active_category_num, socket)
     {:noreply, assign(socket, active_category_num: active_category_num)}
   end
 
@@ -35,14 +32,26 @@ defmodule JeopardyWeb.Components.Trebek.Jeopardy.RevealingBoard do
     {:noreply, socket}
   end
 
-  def get_zipped_categories(assigns) do
+  defp get_zipped_categories(assigns) do
     categories =
-      if assigns.game.status == "jeopardy" do
-        assigns.game.jeopardy_round_categories
-      else
-        assigns.game.double_jeopardy_round_categories
+      case assigns.game.status do
+        "jeopardy" -> assigns.game.jeopardy_round_categories
+        _ -> assigns.game.double_jeopardy_round_categories
       end
 
     Enum.zip(1..6, categories)
+  end
+
+  defp broadcast(active_category_num, socket) do
+    Phoenix.PubSub.broadcast(
+      Jeopardy.PubSub,
+      socket.assigns.game.code,
+      {:next_category,
+       [
+         active_category_num: active_category_num,
+         status: socket.assigns.game.status,
+         round_status: socket.assigns.game.round_status
+       ]}
+    )
   end
 end
