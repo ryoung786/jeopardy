2adcdd324688f22bb81f8473676841bd66671358
Ryan Young <ryany@spotify.com>
2020-05-23 02:28:21 -0400

    somehow missed 2 TV states

--- lib/jeopardy_web/components/tv/pre_jeopardy/selecting_trebek.ex
+++ lib/jeopardy_web/components/tv/pre_jeopardy/selecting_trebek.ex
@@ -1,3 +1,9 @@
-defmodule JeopardyWeb.Components.TV.PreJeopardy.IntroducingRoles do
+defmodule JeopardyWeb.Components.TV.PreJeopardy.SelectingTrebek do
   use JeopardyWeb.Components.Base, :tv
+
+  @impl true
+  def handle_event("trebek_selection", %{"value" => name}, socket) do
+    Jeopardy.Games.assign_trebek(socket.assigns.game, name)
+    {:noreply, socket}
+  end
 end
