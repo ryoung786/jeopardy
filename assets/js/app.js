7eae414acb85cd0747d5a4fceff853f336014850
Ryan Young <ryany@spotify.com>
2020-05-29 18:01:53 -0400

    the phone buzzes when you buzz in!

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,8 +17,15 @@ import {Socket} from "phoenix"
 import NProgress from "nprogress"
 import {LiveSocket} from "phoenix_live_view"
 
+let Hooks = {}
+Hooks.vibratePhone = {
+    mounted() {
+        window.navigator.vibrate([200, 30, 200]);
+    }
+}
+
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, {hooks: Hooks, params: {_csrf_token: csrfToken}})
 
 // Show progress bar on live navigation and form submits
 window.addEventListener("phx:page-loading-start", info => NProgress.start())
