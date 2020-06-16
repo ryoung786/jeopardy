b80dc7f75c444da29c8b8bd6830d0972f074014a
Ryan Young <ryany@spotify.com>
2020-06-16 16:33:36 -0400

    audience fixes

--- lib/jeopardy_web/components/game/pre_jeopardy/awaiting_start.ex
+++ lib/jeopardy_web/components/game/pre_jeopardy/awaiting_start.ex
@@ -1,14 +1,22 @@
 defmodule JeopardyWeb.Components.Game.PreJeopardy.AwaitingStart do
   use JeopardyWeb.Components.Base, :game
+  alias Jeopardy.Games
 
   @impl true
   def update(%{event: :player_joined, name: name}, socket),
-    do: {:ok, assign(socket, audience: [name | socket.assigns.audience])}
+    do: {:ok, assign(socket, audience: socket.assigns.audience ++ [name])}
 
   @impl true
   def update(%{event: :player_left, name: name}, socket),
     do: {:ok, assign(socket, audience: List.delete(socket.assigns.audience, name))}
 
   @impl true
-  def update(assigns, socket), do: {:ok, assign(socket, assigns)}
+  def update(assigns, socket) do
+    audience =
+      socket.assigns[:audience] || Games.get_all_players(assigns.game) |> Enum.map(& &1.name)
+
+    {:ok,
+     assign(socket, assigns)
+     |> assign(audience: audience)}
+  end
 end
--- lib/jeopardy_web/components/tv/pre_jeopardy/selecting_trebek.ex
+++ lib/jeopardy_web/components/tv/pre_jeopardy/selecting_trebek.ex
@@ -1,9 +1,20 @@
 defmodule JeopardyWeb.Components.TV.PreJeopardy.SelectingTrebek do
   use JeopardyWeb.Components.Base, :tv
+  alias Jeopardy.Games
 
   @impl true
   def handle_event("trebek_selection", %{"value" => name}, socket) do
     Jeopardy.Games.assign_trebek(socket.assigns.game, name)
     {:noreply, socket}
+  end
+
+  @impl true
+  def update(assigns, socket) do
+    audience =
+      socket.assigns[:audience] || Games.get_all_players(assigns.game) |> Enum.map(& &1.name)
+
+    {:ok,
+     assign(socket, assigns)
+     |> assign(audience: audience)}
   end
 end
