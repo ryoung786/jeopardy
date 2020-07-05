fdef7ba6d78cb0cbb2e8f40959d0e4be6e61d91f
Ryan Young <ryany@spotify.com>
2020-07-04 23:36:03 -0400

    deleting old dirs

--- lib/jeopardy/engine/pre_jeopardy/selecting_trebek.ex
+++
@@ -1,7 +0,0 @@
-defmodule Jeopardy.Engine.PreJeopardy.SelectingTrebek do
-  use Jeopardy.Engine
-
-  def handle(%{event: :trebek_selected, data: data}, %{game: game} = state) do
-    Jeopardy.Games.assign_trebek(game, data.name)
-  end
-end
