aaff755319bb80afdae0a9378640fe0c870aad0d
Ryan Young <ryany@spotify.com>
2020-08-02 14:48:05 -0400

    click to toggle header user account dropdown; homepage only

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,20 @@
 import "../css/app.scss";
 import { setup } from "./live_view_boilerplate";
 setup({});
+
+window.toggle_user_profile_dropdown = function () {
+  const element = document.querySelector(".header .user-profile-dropdown");
+  element.classList.toggle("active");
+};
+
+window.addEventListener("click", (event) => {
+  if (
+    event.target.closest(".user-profile-dropdown") ||
+    event.target.closest(".user-profile")
+  ) {
+    return;
+  }
+
+  const element = document.querySelector(".header .user-profile-dropdown");
+  element.classList.remove("active");
+});
