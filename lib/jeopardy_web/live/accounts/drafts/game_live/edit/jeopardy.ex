fa31f44e9625f97ca7506517306e1b87621dc952
Ryan Young <ryany@spotify.com>
2020-08-06 19:49:41 -0400

    consolidate into one liveview, rm double-jeopardy

--- lib/jeopardy_web/live/accounts/drafts/game_live/edit/jeopardy.ex
+++ lib/jeopardy_web/live/accounts/drafts/game_live/edit/jeopardy.ex
@@ -4,17 +4,28 @@ defmodule JeopardyWeb.Accounts.Drafts.GameLive.Edit.Jeopardy do
   require Logger
 
   @impl true
-  def handle_params(%{"id" => id, "clue_id" => clue_id}, _url, socket) do
+  def handle_params(%{"id" => id, "clue_id" => clue_id, "round" => round}, _url, socket) do
+    round = String.replace(round, "-", "_")
     game = Drafts.get_game!(id)
     clue = Drafts.get_clue!(game, clue_id)
 
     {:noreply,
      socket
+     |> assign(round: round)
      |> assign(game: game)
      |> assign(clue: Drafts.get_clue!(game, clue_id))}
   end
 
   @impl true
-  def handle_params(%{"id" => id}, _url, socket),
-    do: {:noreply, assign(socket, game: Drafts.get_game!(id))}
+  def handle_params(%{"id" => id, "round" => round}, _url, socket),
+    do:
+      {:noreply,
+       socket
+       |> assign(round: String.replace(round, "-", "_"))
+       |> assign(game: Drafts.get_game!(id))}
+
+  defp edit_path(socket, round, game) do
+    round = String.replace(round, "_", "-")
+    Routes.game_edit_jeopardy_path(socket, :edit, game, round)
+  end
 end
