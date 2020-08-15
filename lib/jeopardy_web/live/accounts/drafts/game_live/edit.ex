349b109a7ced6109f71e443a88fce047a0296140
Ryan Young <ryany@spotify.com>
2020-08-15 17:06:43 -0400

    layout is set

--- lib/jeopardy_web/live/accounts/drafts/game_live/edit.ex
+++ lib/jeopardy_web/live/accounts/drafts/game_live/edit.ex
@@ -1,6 +1,7 @@
 defmodule JeopardyWeb.Accounts.Drafts.GameLive.Edit do
   use JeopardyWeb, :live_view
   alias Jeopardy.Drafts
+  alias Jeopardy.Drafts.Game
   require Logger
 
   @impl true
@@ -8,13 +9,40 @@ defmodule JeopardyWeb.Accounts.Drafts.GameLive.Edit do
       when round in ~w(details jeopardy double-jeopardy final-jeopardy) do
     IO.inspect(round, label: "[xxx] round")
 
+    round = String.replace(round, "-", "_")
+    game = Drafts.get_game!(id)
+    toc_links = toc_links_for_round(game, round)
+    fj_clue = Map.get(game.clues, "final_jeopardy")
+
+    cs = %{
+      details: Drafts.change_game(game),
+      final_jeopardy: Drafts.change_final_jeopardy_clue(%{}, fj_clue)
+    }
+
     {:noreply,
      socket
-     |> assign(game: Drafts.get_game!(id))
-     |> assign(active_tab: String.replace(round, "-", "_"))}
+     |> assign(game: game)
+     |> assign(toc_links: toc_links)
+     |> assign(cs: cs)
+     |> assign(active_tab: round)}
   end
 
   @impl true
   def handle_params(%{"id" => id}, url, socket),
     do: handle_params(%{"id" => id, "round" => "details"}, url, socket)
+
+  defp toc_links_for_round(%Game{} = game, round) do
+    case round do
+      "details" -> ["General Info"]
+      "jeopardy" -> categories(game, round)
+      "double_jeopardy" -> categories(game, round)
+      "final_jeopardy" -> ["Category", "Clue"]
+    end
+  end
+
+  defp categories(%Game{} = game, round) when round in ~w(jeopardy double_jeopardy),
+    do:
+      game.clues[round]
+      |> Enum.with_index()
+      |> Enum.map(fn {cat, i} -> cat["category"] || "Category #{i + 1}" end)
 end
