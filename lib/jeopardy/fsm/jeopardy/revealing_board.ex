9bcae548911f3c08ef61ea0c6384dfb2b090fd43
Ryan Young <ryany@spotify.com>
2020-11-01 17:49:14 -0500

    Fixes #50.  Board control goes to lowest score If there is a tie for the lowest score, break tie by random selection.

--- lib/jeopardy/fsm/jeopardy/revealing_board.ex
+++ lib/jeopardy/fsm/jeopardy/revealing_board.ex
@@ -2,11 +2,15 @@ defmodule Jeopardy.FSM.Jeopardy.RevealingBoard do
   use Jeopardy.FSM
   alias Jeopardy.Repo
   alias Jeopardy.Games.Game
+  alias Jeopardy.Games.Player
   import Ecto.Query
 
   @impl true
   def handle(:next, _, %State{} = state) do
-    {_, %{name: player_name}} = Enum.random(state.contestants)
+    %{name: player_name} =
+      state.contestants
+      |> Enum.map(fn {_id, contestant} -> contestant end)
+      |> player_with_lowest_score()
 
     updates = [
       board_control: player_name,
@@ -17,5 +21,16 @@ defmodule Jeopardy.FSM.Jeopardy.RevealingBoard do
     |> Repo.update_all_ts(set: updates)
 
     {:ok, retrieve_state(state.game.id)}
+  end
+
+  defp player_with_lowest_score(contestants) do
+    # If there is a tie for the lowest score, pick one randomly
+    # So first grab the lowest score
+    %Player{score: lowest_score} = Enum.min_by(contestants, fn c -> c.score end)
+
+    # then choose among players with that score randomly
+    contestants
+    |> Enum.filter(fn c -> c.score == lowest_score end)
+    |> Enum.random()
   end
 end
