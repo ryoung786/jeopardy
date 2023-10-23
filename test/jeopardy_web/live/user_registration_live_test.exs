2b746d3cdd75f2870f8a3852e7961f5e875a1d07
Ryan Young <ryoung786@gmail.com>
2023-10-23 02:01:30 -0400

    Fix auth tests

--- test/jeopardy_web/live/user_registration_live_test.exs
+++ test/jeopardy_web/live/user_registration_live_test.exs
@@ -51,7 +51,6 @@ defmodule JeopardyWeb.UserRegistrationLiveTest do
       conn = get(conn, "/")
       response = html_response(conn, 200)
       assert response =~ email
-      assert response =~ "Settings"
       assert response =~ "Log out"
     end
 
