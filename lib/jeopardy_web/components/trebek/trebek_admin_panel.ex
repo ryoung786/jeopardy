0fee47ae9b21d2d95f5a820c6b508620c4b4681d
Ryan Young <ryoung786@gmail.com>
2023-11-19 01:44:24 -0500

    Edit score from admin panel

--- lib/jeopardy_web/components/trebek/trebek_admin_panel.ex
+++ lib/jeopardy_web/components/trebek/trebek_admin_panel.ex
@@ -3,11 +3,19 @@ defmodule JeopardyWeb.Components.Trebek.TrebekAdminPanel do
   use JeopardyWeb, :live_component
 
   alias Jeopardy.FSM.Messages.ScoreUpdated
+  alias Jeopardy.GameServer
 
   def handle_event("remove-contestant", %{"player_name" => name}, socket) do
     game = socket.assigns.game
     game = update_in(game.contestants, &Map.delete(&1, name))
     {:noreply, assign(socket, game: game)}
+  end
+
+  def handle_event("edit-score", %{"player_name" => name, "score" => score}, socket) do
+    with {score, _} <- Integer.parse(score),
+         do: GameServer.admin_action(socket.assigns.game.code, :set_score, {name, score})
+
+    {:noreply, socket}
   end
 
   def update(%{score_update: %ScoreUpdated{} = msg}, socket) do
@@ -58,6 +66,32 @@ defmodule JeopardyWeb.Components.Trebek.TrebekAdminPanel do
     |> JS.hide(
       to: "#trebek-admin-panel [data-contestant-name='#{name}']",
       transition: {"transition-all ease-out", "opacity-100 translate-y-0", "opacity-0 -translate-y-4"}
+    )
+  end
+
+  def show_edit_score(js \\ %JS{}, name) do
+    js
+    |> JS.transition(
+      {"transition-all ease-in", "opacity-100", "opacity-0"},
+      to: "#trebek-admin-panel [data-contestant-name='#{name}'] > .row"
+    )
+    |> JS.show(
+      to: "#trebek-admin-panel [data-contestant-name='#{name}'] .edit-score",
+      transition: {"transition-all ease-out", "opacity-0", "opacity-100"},
+      display: "flex"
+    )
+    |> JS.focus(to: "#trebek-admin-panel [data-contestant-name='#{name}'] .score-input")
+  end
+
+  def hide_edit_score(js \\ %JS{}, name) do
+    js
+    |> JS.transition(
+      {"transition-all ease-out", "opacity-0", "opacity-100"},
+      to: "#trebek-admin-panel [data-contestant-name='#{name}'] > .row"
+    )
+    |> JS.hide(
+      to: "#trebek-admin-panel [data-contestant-name='#{name}'] .edit-score",
+      transition: {"transition-all ease-in", "opacity-100", "opacity-0"}
     )
   end
 end
